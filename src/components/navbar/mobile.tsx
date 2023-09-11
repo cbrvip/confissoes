6a34a86096c63ffe96dbcc29a4f7aaa94f0b72f8
Gustavo Colhado <gustavocolhado@gmail.com>
2023-09-11 17:06:47 -0300

    correção nav mobile

--- src/components/navbar/mobile.tsx
+++ src/components/navbar/mobile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './index.scss'
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
@@ -11,12 +11,32 @@ const NavMobile: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { user, signed, loadingAuth } = useContext(AuthContext);
 
-    async function handleLogout() {
-        await signOut(auth);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
     }
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  async function handleLogout() {
+    await signOut(auth);
+  }
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
   };
 
   return (
@@ -26,34 +46,48 @@ const NavMobile: React.FC = () => {
         <div className="bar"></div>
         <div className="bar"></div>
       </div>
-      <nav className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+      <nav ref={menuRef} className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
         <ul>
-        <li><a href="/"><span><FaHouseChimney size={24} /></span> Página Inicial</a></li>
-                    {!loadingAuth && signed && (
-                    <Link to={`/profile/${user?.uid}`}>
-                        <li><span><FaUser size={24} /></span> Meu Perfil</li>
-                    </Link>
-                    )}
-                    {!loadingAuth && signed && (
-                    <Link to={"/profile/post"}>
-                        <li><span><FaPhotoFilm size={24} /></span> Novo Post</li>
-                    </Link>
-                    )}
-                    {!loadingAuth && signed && (
-                    <Link to={"/"} onClick={handleLogout}>
-                        <li><span><FaCircleLeft size={24} /></span> Logout</li>
-                    </Link>
-                    )}
-                    {!loadingAuth && !signed && (
-                        <Link to={"/login"}>
-                            <li><span><FaUser size={24} /></span> Login</li>
-                        </Link>
-                    )}
-                    {!loadingAuth && !signed && (
-                        <Link to={"/register"}>
-                            <li><span><FaUserPlus size={24} /></span> Cadastrar-se</li>
-                        </Link>
-                    )}
+          <li>
+            <Link to="/" onClick={closeMenu}>
+              <span><FaHouseChimney size={24} /></span> Página Inicial
+            </Link>
+          </li>
+          {!loadingAuth && signed && (
+            <li>
+              <Link to={`/profile/${user?.uid}`} onClick={closeMenu}>
+                <span><FaUser size={24} /></span> Meu Perfil
+              </Link>
+            </li>
+          )}
+          {!loadingAuth && signed && (
+            <li>
+              <Link to="/profile/post" onClick={closeMenu}>
+                <span><FaPhotoFilm size={24} /></span> Novo Post
+              </Link>
+            </li>
+          )}
+          {!loadingAuth && signed && (
+            <li>
+              <Link to="/" onClick={() => { closeMenu(); handleLogout(); }}>
+                <span><FaCircleLeft size={24} /></span> Logout
+              </Link>
+            </li>
+          )}
+          {!loadingAuth && !signed && (
+            <li>
+              <Link to="/login" onClick={closeMenu}>
+                <span><FaUser size={24} /></span> Login
+              </Link>
+            </li>
+          )}
+          {!loadingAuth && !signed && (
+            <li>
+              <Link to="/register" onClick={closeMenu}>
+                <span><FaUserPlus size={24} /></span> Cadastrar-se
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </>
