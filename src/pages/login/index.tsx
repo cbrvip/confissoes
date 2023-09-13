3c42f92ae4cd0ed81ab7dc4fbc076fdf968c3edc
Gustavo Colhado <gustavocolhado@gmail.com>
2023-09-13 16:32:05 -0300

    alteraçoes login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,6 +10,7 @@ import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../../services/firebaseConnection';
 import toast from 'react-hot-toast';
 import { Header } from '../../components/header';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const schema = z.object({
   email: z.string().email("Insira um email válido").nonempty("O campo email é obrigatório"),
@@ -34,66 +35,92 @@ export function Login() {
     handleLogout();
   }, []);
 
-  function onSubmit(data: FormData) {
-    signInWithEmailAndPassword(auth, data.email, data.password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        toast.success("Logado com sucesso!");
-        navigate(`/`, { replace: true });
-      })
-      .catch(err => {
-        toast.error("Erro ao fazer o login", err);
-      });
+  async function getUsernameFromDatabase(uid: string) {
+    const db = getFirestore();
+    const userRef = doc(db, 'users', uid);
+  
+    try {
+      const userDoc = await getDoc(userRef);
+  
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        return userData.username;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error('Erro ao buscar nome de usuário:', error);
+      return null;
+    }
   }
+
+  async function onSubmit(data: FormData) {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, data.email, data.password);
+    const user = userCredential.user;
+
+    const username = await getUsernameFromDatabase(user.uid);
+
+    toast.success("Logado com sucesso!");
+
+    if (username) {
+      navigate(`/profile/${username}`, { replace: true });
+    } else {
+
+    }
+  } catch (err) {
+    toast.error("Erro ao fazer o login");
+  }
+}
 
   return (
     <>
-    <Header />
-    <Container>
-      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
-        <Link to="/" className="mb-6 max-w-sm w-full">
-          <img
-            className="w-full"
-            src={logoImg}
-            alt="Logo do site"
-          />
-        </Link>
-        <form
-          className="bg-white max-w-xl w-full rounded-lg p-4"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="mb-3">
-            <Input
-              type="email"
-              placeholder="Digite seu email..."
-              name="email"
-              error={errors.email?.message}
-              register={register}
+      <Header />
+      <Container>
+        <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+          <Link to="/" className="mb-6 max-w-sm w-full">
+            <img
+              className="w-full"
+              src={logoImg}
+              alt="Logo do site"
             />
-          </div>
-
-          <div className="mb-3">
-            <Input
-              type="password"
-              placeholder="Digite sua senha..."
-              name="password"
-              error={errors.password?.message}
-              register={register}
-            />
-          </div>
-
-          <button
-            className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium"
-            type="submit"
+          </Link>
+          <form
+            className="bg-white max-w-xl w-full rounded-lg p-4"
+            onSubmit={handleSubmit(onSubmit)}
           >
-            Acessar
-          </button>
-        </form>
-        <Link to="/">
-          Voltar para página inicial
-        </Link>
-      </div>
-    </Container>
+            <div className="mb-3">
+              <Input
+                type="email"
+                placeholder="Digite seu email..."
+                name="email"
+                error={errors.email?.message}
+                register={register}
+              />
+            </div>
+
+            <div className="mb-3">
+              <Input
+                type="password"
+                placeholder="Digite sua senha..."
+                name="password"
+                error={errors.password?.message}
+                register={register}
+              />
+            </div>
+
+            <button
+              className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium"
+              type="submit"
+            >
+              Acessar
+            </button>
+          </form>
+          <Link to="/">
+            Voltar para página inicial
+          </Link>
+        </div>
+      </Container>
     </>
   );
 }
