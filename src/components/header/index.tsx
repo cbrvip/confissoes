7e0375dd799bd9e294d51c2d57d65c80c8655dc6
Gustavo Colhado <gustavocolhado@gmail.com>
2023-09-13 01:41:59 -0300

    alteração header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,16 @@
 import './index.scss';
 import NavMobile from '../navbar/mobile';
 import logoImg from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 export function Header() {
     return(
         <div className="header">
         <NavMobile />
         <div className="logo">
+            <Link to={`/`}>
                 <img src={logoImg} alt="" />
+            </Link>
             </div>
         </div>
     )
