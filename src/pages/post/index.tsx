7fac37cbd5507ec8c9d73198ab842059e6239615
Gustavo Colhado <gustavocolhado@gmail.com>
2023-09-18 11:47:04 -0300

    alt

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -65,28 +65,12 @@ export function PostDetail() {
   const [postOwner, setPostOwner] = useState<string | null>(null);
   const [postOwnerPhoto, setPostOwnerPhoto] = useState<string | null>(null);
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const scrollPositionRef = useRef<number | null>(null);
-
-  useEffect(() => {
-    return () => {
-      // Salvar a posição de rolagem quando o componente for desmontado
-      if (window && window.scrollY) {
-        scrollPositionRef.current = window.scrollY;
-      }
-    };
-  }, []);
 
   useEffect(() => {
     async function loadPost() {
       if (!id) {
         return;
       }
-
-      if (scrollPositionRef.current !== null) {
-        window.scrollTo(0, scrollPositionRef.current);
-        scrollPositionRef.current = null; // Limpar a posição após restaurar
-      }
-    
       const postRef = doc(db, "posts", id);
       const postSnapshot = await getDoc(postRef);
     
@@ -260,8 +244,7 @@ export function PostDetail() {
   }, [id, navigate]);
 
   const handleNavigateToDetail = () => {
-    scrollPositionRef.current = window.scrollY;
-    navigate(`/post/${id}`);
+    navigate(-1);
   };
 
   return (
